177bed5f332d496fe3567f4ff733e48514b84872
Marschell01 <dinhof.marcel@student.htlwrn.ac.at>
2022-03-05 01:14:26 -0800

    Add command line arguments for ip and port

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,33 @@
-
 #include <iostream>
 #include <thread>
+#include <vector>
 
 #include "redis_client.h"
+#include "CLI11.hpp"
 
-int main() {
-    Redis::RedisClient client("localhost", "6379");
+
+int main(int argc, char* argv[]) {
+    std::string ip_address{"localhost"};
+    std::string port{"6379"};
+
+    CLI::App app("A simple redis client");
+    app.add_option("--ip", ip_address, "ip address to connect to")->check(
+      [](const std::string &str) {
+            asio::error_code ec;
+            asio::ip::make_address(str, ec);
+            if (ec.value() != 0 && str != "localhost") {
+                return std::string("Error: " + str + " invalid ip address!");
+            }
+            return std::string();
+        }  
+    );
+    app.add_option("-p,--port", port, "port to connect to");
+
+    CLI11_PARSE(app, argc, argv); 
+
+    Redis::RedisClient client(ip_address, port);
     client.set("Name", "MaxMuster");
-    
+
     std::cout << client.get("Nahme") << "\n";
 
     std::cout << client.get("Name") << std::endl;

e7e3af3117bfe0db2b40f28e900e55c74716c7a0
Marschell01 <dinhof.marcel@student.htlwrn.ac.at>
2022-03-23 20:57:51 +0100

    Refactor connection class

--- include/redis_connection.hpp
+++ include/redis_connection.hpp
@@ -3,6 +3,7 @@
 #include <asio.hpp>
 #include <iostream>
 #include <string>
+#include <vector>
 #include "logger.h"
 
 namespace Redis {
@@ -16,12 +17,10 @@ namespace Redis {
     public:
         RedisConnection(std::string ip_address, std::string port) {
             LOG_INFO("Try to connect to server!");
-            try {
-                auto results = resolver.resolve(ip_address, port);
-                asio::connect(socket, results);
-            } catch (asio::system_error& e) {
-                LOG_ERROR(e.what());
-            }
+            
+            auto results = resolver.resolve(ip_address, port);
+            asio::connect(socket, results);
+            
             LOG_INFO("Connected to server!");
         }
 
@@ -30,24 +29,33 @@ namespace Redis {
             LOG_INFO("Connection closed");
         }
 
-        std::string getData() {
+        std::vector<std::string> getData() {
+            std::vector<std::string> reply{};
+            std::string temp{};
+
             if (socket.is_open()) {
                 asio::streambuf buf;
-                LOG_DEBUG("Before read");
-                asio::read_until(socket, buf, '\n');
-                LOG_DEBUG("After read!");
-                std::string reply;
-                LOG_DEBUG("Creating stream!");
-                std::istream is{&buf};
-                LOG_DEBUG("Created stream!");
 
-                LOG_DEBUG("Getting line!");
-                getline(is, reply);
-                LOG_DEBUG("Got line!");
+                LOG_DEBUG("Before read");
+
+                asio::read_until(socket, buf, '\n');
+
+                LOG_DEBUG("After read!");
+                LOG_DEBUG("Creating stream!");
+
+                std::istream is{&buf};
+
+                LOG_DEBUG("Created stream!");
+                LOG_DEBUG("Processing received message!");
+                while (std::getline(is, temp)) {
+                    reply.push_back(temp);
+                }
+
+                LOG_DEBUG("Processed received message!");
                 return reply;
             }
             LOG_ERROR("Can not get data, socket is closed!");
-            return "";
+            return reply;
         }
                     
 

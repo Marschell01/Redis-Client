ea5564b647b56cab6538448c8d1f38c17a51bc57
Marschell01 <dinhof.marcel@student.htlwrn.ac.at>
2022-03-26 20:55:35 +0100

    Add print functions and testing for arrays and maps

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,58 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <type_traits>
+#include <variant>
+#include <vector>
 
 #include "redis_execute.hpp"
 #include "redis_types.h"
 #include "CLI11.hpp"
+
+void print_map(Redis::Map& map);
+void print_array(Redis::Array& array);
+
+void print_helper(const std::string& key, Redis::redis_types* val) {
+    if(val->index() == 0) { //SimpleString
+        Redis::SimpleString value = std::get<Redis::SimpleString>(*val);
+        LOG_INFO("{0} => {1}", key, value.get());
+    }
+    if(val->index() == 1) { //BulkString
+        Redis::BulkString value = std::get<Redis::BulkString>(*val);
+        LOG_INFO("{0} => {1}", key, value.get());
+    }
+    if(val->index() == 2) { //Integer
+        Redis::Integer value = std::get<Redis::Integer>(*val);
+        LOG_INFO("{0} => {1}", key, value.get());
+    }
+    if(val->index() == 3) { //Integer
+        Redis::Array value = std::get<Redis::Array>(*val);
+        LOG_INFO("{0} => *", key);
+        print_array(value);
+    }
+    if(val->index() == 4) { //Integer
+        Redis::Map value = std::get<Redis::Map>(*val);
+        LOG_INFO("{0} => %", key);
+        print_map(value);
+    }
+}
+
+void print_map(Redis::Map& map) {
+    for (auto& [key, val] : map.get()) {
+        print_helper(key, val.get());
+    }
+}
+
+void print_array(Redis::Array& array) {
+    int idx_counter{0};
+    for (auto& elem : array.get()) {
+        print_helper(std::to_string(idx_counter), elem.get());
+        idx_counter++;
+    }
+}
 
 int main(int argc, char* argv[]) {
     std::string ip_address{"localhost"};
@@ -55,6 +103,13 @@ int main(int argc, char* argv[]) {
     LOG_INFO(Redis::SimpleString(output).get());
     output = Redis::execute(*con, "GET", "name");
     LOG_INFO(Redis::BulkString(output).get());
+
+
+    output = Redis::execute(*con, "HELLO", "3");
+    Redis::Map map{output};
+
+    print_map(map);
+    
     //END - Default test
 
     delete con;

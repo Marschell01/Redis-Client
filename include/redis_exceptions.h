17562ca93127cfbf588513c006800df9b8f42510
Marschell01 <dinhof.marcel@student.htlwrn.ac.at>
2022-04-04 17:16:06 +0200

    Remove unnecessary redis exception class

--- include/redis_exceptions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <exception>
-
-struct RedisResponseException : public std::exception {
-   const char * what () const throw () {
-      return "Invalid response from redis server";
-   }
-};

e8e002fc363b0836e091a90f822f085361cf454c
Marschell01 <dinhof.marcel@student.htlwrn.ac.at>
2022-04-10 14:11:31 +0200

    Update README.md and main.cpp for final version

--- README.md
+++ README.md
@@ -1,1 +1,118 @@
 # dinhof_project
+## Ein einfacher Redis Client in C++
+
+Dieser Client ermöglicht eine flexieble Kommunikation mit einem Redis Server.
+Aufgrund seiner Klassenstruktur kann er beleibig erweitern und angepasst werden.
+
+
+## Installation
+
+
+```bash
+git clone https://github.com/Marschell01/dinhof_project.git
+cd dinhof_project/
+meson build && cd build
+ninja   
+```
+
+## Ausführung
+Der Client funktioniert nur mit der bereitgestellten Proxy. Diese empfängt die Daten des Clients und sendet diese an einen Redis Server weiter.
+
+Die Proxy wird wie folgt gestartet:
+```bash
+./redis_proxy --dport {Port des Redis Servers} --hport {Port, auf dem die Proxy nach einem Client lauschen soll}
+```
+
+Der Client wird wie folgt gestartet:
+```bash
+./redis_client --ip {IP Adresse des Proxy Servers} --port {Port des Proxy Servers}
+```
+
+## Bedienung
+
+```cpp
+/*
+ * Kommunikation mit dem Server
+*/
+Redis::RedisClient client{ip_address, destination_port};
+if (!client.is_connected()) {
+    //error handling
+    return 1;
+}
+
+std::string output;
+output = client.execute("SET", "name", "MaxMuster123").parse<std::string>();
+output = client.execute("GET", "name").parse<std::string>();
+
+
+/*
+ * Pipelining
+*/
+client.execute_no_flush("SET", "name", "MaxMuster321");
+client.execute_no_flush("GET", "name");
+std::vector<Redis::RedisResponse> responses{client.flush_pending()};
+
+for (Redis::RedisResponse& response : responses) {
+    LOG_INFO(response.parse<std::string>() << std::endl);
+}
+
+/*
+ * Transaktionen
+*/
+output = client.execute("SET", "name", "MaxMuster321").parse<std::string>();
+LOG_INFO(output);
+
+client.begin_transaction();
+output = client.execute("SET", "name", "MaxMuster123").parse<std::string>();
+LOG_INFO(output);
+
+output = client.execute("GET", "name").parse<std::string>();
+LOG_INFO(output);
+    
+//client.end_transaction();      -> Transaktion wird durchgeführt
+client.discard_transaction(); // -> Transaktion wird abgebrochen
+
+output = client.execute("GET", "name").parse<std::string>();
+LOG_INFO(output);
+
+
+/*
+ * Locks
+*/
+client.lock("resource_1");
+
+output = client.execute("SET", "name", "MaxMuster123").parse<std::string>();
+LOG_INFO(output);
+
+output = client.execute("GET", "name").parse<std::string>();
+LOG_INFO(output);
+
+client.unlock("resource_1");
+
+
+/*
+ * Subscribe
+*/
+client.subscribe("subscribe_object");
+while (true) {
+    LOG_INFO("Subscriber waiting for data");
+        
+    std::vector<Redis::RedisResponse> responses{client.fetch_data()};
+    for (Redis::RedisResponse& response : responses) {
+        LOG_INFO(response.parse<std::string>());
+    }
+}
+
+
+/*
+ * Publish
+*/
+std::string input{};
+while (true) {
+    std::cout << "Element: ";
+    std::getline(std::cin, input);
+    output = client.execute("PUBLISH", "subscribe_object", input).parse<std::string>();
+    LOG_INFO(output);
+}
+```
+Falls ein Rückgabewert nicht in den angegebenen Datentypen geschpeichert werden kann, tritt eine Exception auf.
--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,6 @@
 int main(int argc, char* argv[]) {
     std::string ip_address{"localhost"};
     int destination_port{6379};
-    bool sub{false};
-    bool pub{false};
     
     CLI::App app("A simple redis client");
     app.add_option("--ip", ip_address, "ip address to connect to")->check(
@@ -23,107 +21,9 @@ int main(int argc, char* argv[]) {
         }  
     )->required();
     app.add_option("-p,--port", destination_port, "port to connect to")->required();
-    app.add_flag("--sub", sub, "set client to subscriber");
-    app.add_flag("--pub", pub, "set client to publisher");
 
     //LOG_SET_LOGLEVEL(LOG_LEVEL_DEBUG);
 
     CLI11_PARSE(app, argc, argv); 
-
-    Redis::RedisClient client{ip_address, destination_port};
-    if (!client.is_connected()) {
-        LOG_ERROR("Proxy unavailable!");
-        return 1;
-    }
-
-    /*
-    std::string output;
-    output = client.execute("SET", "name", "MaxMuster123").parse<std::string>();
-    LOG_INFO(output);
-
-    output = client.execute("GET", "name").parse<std::string>();
-    LOG_INFO(output);
-
-    output = client.execute("HELLO", "3").parse<std::string>();
-    LOG_INFO(output);
-    */
-    
-    /*
-    std::string output;
-    client.execute_no_flush("SET", "name", "MaxMuster321");
-    client.execute_no_flush("GET", "name");
-    std::vector<Redis::RedisResponse> responses{client.flush_pending()};
-
-    for (Redis::RedisResponse& response : responses) {
-        LOG_INFO(response.parse<std::string>());
-    }
-    */
-
-    /*
-    std::string output;
-    client.lock("resource_1");
-
-    try {
-        std::this_thread::sleep_for(std::chrono::milliseconds{5000});
-        output = client.execute("SET", "name", "MaxMuster123").parse<std::string>();
-        LOG_INFO(output);
-
-        output = client.execute("GET", "name").parse<std::string>();
-        LOG_INFO(output);
-        std::this_thread::sleep_for(std::chrono::milliseconds{2000});
-    } catch(std::invalid_argument& e) {
-        LOG_ERROR(e.what());
-    }
-
-    client.unlock("resource_1");
-    */
-    /*
-    std::string output;
-    try {
-        output = client.execute("SET", "name", "MaxMuster321").parse<std::string>();
-        LOG_INFO(output);
-
-        client.begin_transaction();
-
-        output = client.execute("SET", "name", "MaxMuster123").parse<std::string>();
-        LOG_INFO(output);
-
-        output = client.execute("GET", "name").parse<std::string>();
-        LOG_INFO(output);
-    
-        //client.end_transaction();
-        client.discard_transaction();
-
-        output = client.execute("GET", "name").parse<std::string>();
-        LOG_INFO(output);
-    } catch(std::invalid_argument& e) {
-        LOG_ERROR(e.what());
-    }
-    */
-
-    std::string output;
-    if (sub) {
-        client.subscribe("subscribe_object");
-        while (true) {
-            LOG_INFO("Subscriber waiting for data");
-            
-            std::vector<Redis::RedisResponse> responses{client.fetch_data()};
-
-            for (Redis::RedisResponse& response : responses) {
-                LOG_INFO(response.parse<std::string>());
-            }
-            
-        }
-    }
-    
-    if (pub) {
-        std::string input{};
-        while (true) {
-            std::cout << "Element: ";
-            std::getline(std::cin, input);
-            output = client.execute("PUBLISH", "subscribe_object", input).parse<std::string>();
-            LOG_INFO(output);
-        }
-    }
     return 0;
 }

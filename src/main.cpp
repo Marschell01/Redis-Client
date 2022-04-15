7e37072342bf5d608dfa2e2379836b263f3dbf71
Marschell01 <dinhof.marcel@student.htlwrn.ac.at>
2022-04-15 10:01:50 +0200

    Finish user interface for redis client

--- src/main.cpp
+++ src/main.cpp
@@ -50,12 +50,15 @@ std::string interaction(const std::string& ip, const int& port, Redis::RedisClie
 
 int main(int argc, char* argv[]) {
     std::string ip_address{"localhost"};
+    std::string lock_resource{""};
+    std::string subscribe_to{""};
+    std::string publish_to{""};
     int destination_port{6379};
-    bool interactive_mode_on{false};
-    std::vector<std::string> commands;
     int wait_time{0};
     int re_run_count{1};
+    bool interactive_mode_on{false};
     bool transaction_on{false};
+    std::vector<std::string> commands;
 
     
     CLI::App app("A simple redis client");
@@ -70,11 +73,14 @@ int main(int argc, char* argv[]) {
         }  
     );
     app.add_option("-p,--port", destination_port, "port to connect to");
-    app.add_flag("-i,--interactive", interactive_mode_on, "enable of disable interactive mode");
     app.add_option("-c,--command", commands, "Command to execute");
     app.add_option("-r,--redo", re_run_count, "Excpects amout of itterations for command");
-    app.add_option("--wait", wait_time, "Specify milliseconds between command call");
-    app.add_flag("-t,--transaction", transaction_on ,"Starts a transaction");
+    app.add_option("-w,--wait", wait_time, "Specify milliseconds between command call");
+    app.add_option("--subscribe", subscribe_to, "Subscribes to a specific resource");
+    app.add_option("--publish", publish_to, "Publishes to a specific resource in interactive mode");
+    app.add_option("-l,--lock", lock_resource ,"Sets a lock using a temporary resource and starts interactive mode");
+    app.add_flag("-i,--interactive", interactive_mode_on, "Enable of disable interactive mode");
+    app.add_flag("-t,--transaction", transaction_on ,"Starts a transaction and enables interactive mode");
 
     //LOG_SET_LOGLEVEL(LOG_LEVEL_DEBUG);
 
@@ -85,6 +91,31 @@ int main(int argc, char* argv[]) {
     std::string output;
 
     if (!interactive_mode_on) {
+
+        if (subscribe_to.size() > 0) {
+            client.subscribe(subscribe_to);
+            while (client.is_connected()) {
+                LOG_INFO(client.is_connected());
+                LOG_INFO("Subscriber waiting for data");
+                    
+                std::vector<Redis::RedisResponse> responses{client.fetch_data()};
+                for (Redis::RedisResponse& response : responses) {
+                    LOG_INFO(response.parse<std::string>());
+                }
+            }
+            return 0;
+        }
+
+        if (publish_to.size() > 0) {
+            LOG_INFO("Start publishing to: {0}", publish_to)
+            while (true) {
+                std::cout << "Element: ";
+                std::getline(std::cin, input);
+                output = client.execute("PUBLISH", publish_to, input).parse<std::string>();
+                LOG_INFO(output);
+            }
+            return 0;
+        }
 
         if (transaction_on) {
             client.begin_transaction();
@@ -100,14 +131,22 @@ int main(int argc, char* argv[]) {
             } 
         }
 
+        if (lock_resource.size() > 0) {
+            client.lock(lock_resource);
+            LOG_INFO("Type -end to exit");
+            interaction(ip_address, destination_port, client);
+            client.unlock(lock_resource);
+            return 0;
+        }
+
         if (commands.size() > 0) {
             for (int i{0}; i < re_run_count; i++) {
                 output = client.execute(commands).parse<std::string>();
                 LOG_INFO(output);
                 std::this_thread::sleep_for(std::chrono::milliseconds{wait_time});
             }
+            return 0;
         }
-        return 0;
     }
 
     //Interactive Mode
